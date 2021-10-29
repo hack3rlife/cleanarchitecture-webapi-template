c16a5f1e0c6aee33bc84887ea9988d6e6e8342a0
hack3rlife <hack3rlifeio@gmail.com>
2021-10-28 21:25:39 -0700

    Adding template parameter support to replace project name

--- Template/src/PROJECT_NAME.Application/ServiceCollectionConfiguration.cs
+++ Template/src/PROJECT_NAME.Application/ServiceCollectionConfiguration.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CleanArchitecture.Infrastructure
+namespace PROJECT_NAME.Application
 {
     public static class ServiceCollectionConfiguration
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             return services;
         }
